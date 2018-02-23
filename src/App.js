aa22a0306cd6f4f643b02c8f6ba8ff93f205a633
Silvester Garcia <ing.silvestre.garcia@gmail.com>
2018-02-22 18:48:29 -0600

    Se cambia a productos de mi pagina

--- src/App.js
+++ src/App.js
@@ -1,111 +1,16 @@
-import React, { Component, Fragment } from 'react';
-import logo from './logo.png';
-import methods from './methods_statement_cards.png';
-import oxxopay from './oxxopay.svg';
-import paypal from './paypal.svg';
-import './App.css';
-import Switch from 'react-router-dom/es/Switch';
-import Route from 'react-router-dom/es/Route';
-import Redirect from 'react-router-dom/es/Redirect';
-import BrowserRouter from 'react-router-dom/es/BrowserRouter';
+import React, { Component, Fragment } from "react";
+import logo from "./logo.png";
+import methods from "./methods_statement_cards.png";
+import oxxopay from "./oxxopay.svg";
+import paypal from "./paypal.svg";
+import "./App.css";
+import Switch from "react-router-dom/es/Switch";
+import Route from "react-router-dom/es/Route";
+import Redirect from "react-router-dom/es/Redirect";
+import BrowserRouter from "react-router-dom/es/BrowserRouter";
 
 class App extends Component {
   render() {
-<<<<<<< HEAD
-    const styles = {
-      itemContainer: {
-        display: 'flex',
-        height: '50rem',
-        paddingTop: '5rem',
-        paddingLeft: '15rem',
-        paddingRight: '10rem'
-      },
-      navContainer: {
-        position: 'fixed',
-        padding: '1rem',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '4rem',
-        width: '100%',
-        backgroundColor: 'white',
-        borderBottom: '1px solid rgba(0,0,0,.1)'
-      },
-      navLogo: {
-        height: '80%'
-      },
-      navLinksContainer: {
-        display: 'flex',
-        paddingRight: '4rem'
-      },
-      navLink: {
-        fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-        fontWeight: '300',
-        fontSize: '1.1rem',
-        wordWrap: 'break-word',
-        textDecoration: 'none',
-        color: 'inherit',
-        marginRight: '1rem'
-      },
-      imageContainer: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        height: '100%',
-        flexGrow: 3
-      },
-      itemImage: {
-        height: '80%',
-        width: 'auto'
-      },
-      paymentImage: {
-        height: '30%',
-        width: '30%',
-        padding: '1rem',
-      },
-      detailsContainer: {
-        paddingLeft: '6rem',
-        paddingTop: '4rem',
-        overflow: 'auto',
-        flexGrow: 7
-      },
-      paymentContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-      },
-      title: {
-        fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-        fontWeight: '300',
-        fontSize: '2rem',
-        wordWrap: 'break-word'
-      },
-      subtitle: {
-        fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-        fontWeight: '400',
-        fontSize: '1.1rem',
-        wordWrap: 'break-word'
-      },
-      details: {
-        fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-        fontWeight: '300',
-        fontSize: '1.1rem',
-        wordWrap: 'break-word'
-      },
-      price: {
-        fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-        fontSize: '1.2rem',
-        fontWeight: '400',
-        wordWrap: 'break-word'
-      },
-      shipping: {
-        fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-        fontSize: '1.3rem',
-        fontWeight: '500',
-        wordWrap: 'break-word'
-      }
-=======
     return (
       <BrowserRouter>
         <Switch>
@@ -123,114 +28,115 @@ export default App;
 class Item extends Component {
   styles = {
     itemContainer: {
-      display: 'flex',
-      height: '50rem',
-      paddingTop: '5rem',
-      paddingLeft: '15rem',
-      paddingRight: '10rem'
+      display: "flex",
+      height: "50rem",
+      paddingTop: "5rem",
+      paddingLeft: "15rem",
+      paddingRight: "10rem"
     },
     navContainer: {
-      position: 'fixed',
-      padding: '1rem',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      height: '4rem',
-      width: '100%',
-      backgroundColor: 'white',
-      borderBottom: '1px solid rgba(0,0,0,.1)'
+      position: "fixed",
+      padding: "1rem",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      height: "4rem",
+      width: "100%",
+      backgroundColor: "white",
+      borderBottom: "1px solid rgba(0,0,0,.1)"
     },
     navLogo: {
-      height: '80%'
+      height: "80%"
     },
     navLinksContainer: {
-      display: 'flex',
-      paddingRight: '4rem'
+      display: "flex",
+      paddingRight: "4rem"
     },
     navLink: {
       fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-      fontWeight: '300',
-      fontSize: '1.1rem',
-      wordWrap: 'break-word',
-      textDecoration: 'none',
-      color: 'inherit',
-      marginRight: '1rem'
+      fontWeight: "300",
+      fontSize: "1.1rem",
+      wordWrap: "break-word",
+      textDecoration: "none",
+      color: "inherit",
+      marginRight: "1rem"
     },
     imageContainer: {
-      display: 'flex',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-      height: '100%',
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "center",
+      height: "100%",
       flexGrow: 3
     },
     itemImage: {
-      height: '80%',
-      width: 'auto'
+      height: "80%",
+      width: "auto"
     },
     detailsContainer: {
-      paddingLeft: '6rem',
-      paddingTop: '4rem',
-      overflow: 'auto',
+      paddingLeft: "6rem",
+      paddingTop: "4rem",
+      overflow: "auto",
       flexGrow: 7
     },
     title: {
       fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-      fontWeight: '300',
-      fontSize: '2rem',
-      wordWrap: 'break-word'
+      fontWeight: "300",
+      fontSize: "2rem",
+      wordWrap: "break-word"
     },
     description: {
       fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-      fontWeight: '400',
-      fontSize: '1.1rem',
-      wordWrap: 'break-word'
+      fontWeight: "400",
+      fontSize: "1.1rem",
+      wordWrap: "break-word"
     },
     details: {
       fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-      fontWeight: '300',
-      fontSize: '1.1rem',
-      wordWrap: 'break-word'
+      fontWeight: "300",
+      fontSize: "1.1rem",
+      wordWrap: "break-word"
     },
     price: {
       fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-      fontSize: '1.2rem',
-      fontWeight: '400',
-      wordWrap: 'break-word'
+      fontSize: "1.2rem",
+      fontWeight: "400",
+      wordWrap: "break-word"
     },
     shipping: {
       fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
-      fontSize: '1.3rem',
-      fontWeight: '500',
-      wordWrap: 'break-word'
+      fontSize: "1.3rem",
+      fontWeight: "500",
+      wordWrap: "break-word"
     }
   };
   products = [
     {
-      id: 'producto1',
-      title: 'Camisa 1',
-      description: 'Descripción',
-      price: 100.0,
+      id: "1",
+      title: "Cheesecake de caramelo",
+      description: "Cheesecake con un toque dulce y fácil de hacer",
+      price: 280.0,
       image:
-        'https://vangogh.teespring.com/v3/image/oGRwVyoQlFfX-UDpYHwSIjMuDa0/480/560.jpg',
-      shipping: 'No shipping'
+        "http://lareposteriagdl.com/wp-content/uploads/2018/01/descarga.jpg",
+      shipping: "No shipping"
     },
     {
-      id: 'producto2',
-      title: 'Camisa 2',
-      description: 'Descripción',
-      price: 100.0,
-      image:
-        'https://vangogh.teespring.com/v3/image/oGRwVyoQlFfX-UDpYHwSIjMuDa0/480/560.jpg',
-      shipping: 'No shipping'
-    },
-    {
-      id: 'cat',
-      title: 'My Cat Can Live A Better Life Shirt',
+      id: "2",
+      title: "Pastel de zanahoria",
       description:
-        'I Work Hard So My Cat Can Live A Better Life Shirt, Funny Cat Shirt',
-      price: 100.0,
+        "¿Quién habría adivinado que una piña, compota de manzana y zanahorias podrían ser parte de un postre tan sabroso?",
+      price: 300.0,
       image:
-        'https://vangogh.teespring.com/v3/image/oGRwVyoQlFfX-UDpYHwSIjMuDa0/480/560.jpg',
+        "http://lareposteriagdl.com/wp-content/uploads/2018/01/pastel_zanahoria.jpg",
+      shipping: "No shipping"
+    },
+    {
+      id: "3",
+      title: "Torta Rellena Cremosa",
+      description:
+        "¡La combinación perfecta del dulce en un pastel esponjoso y sabroso!",
+      price: 200.0,
+      image:
+        "http://lareposteriagdl.com/wp-content/uploads/2018/01/torta_rellena_cremosa.png",
       shipping: `Orders are printed and shipped when the time expires or earlier.
               You can expect your package to arrive 10 - 13 business days after
               the product prints. Expedited or Rush shipping may be available
@@ -244,7 +150,6 @@ class Item extends Component {
     const { styles, products } = this;
     const product = products.find(p => p.id == this.props.match.params.id) || {
       ...products[Math.floor(Math.random() * products.length)]
->>>>>>> master
     };
     return (
       <Fragment>
@@ -271,24 +176,7 @@ class Item extends Component {
             <p style={styles.description}>{product.description}</p>
             <p style={styles.price}>${product.price} MXN</p>
             <p style={styles.shipping}>Shipping Info</p>
-<<<<<<< HEAD
-            <p style={styles.details}>
-              Orders are printed and shipped when the time expires or earlier.
-              You can expect your package to arrive 10 - 13 business days after
-              the product prints. Expedited or Rush shipping may be available
-              depending upon the product(s) selected and the destination
-              country. Shipping costs start at: $3.99 for the first apparel item
-              and $2.00 for each additional apparel item. Products are fulfilled
-              in the US
-            </p>
-            <div style={styles.paymentContainer}>
-              <img style={styles.paymentImage} src={methods} alt="methods" />
-              <img style={styles.paymentImage} src={oxxopay} alt="oxxopay" />
-              <img style={styles.paymentImage} src={paypal} alt="paypal" />
-            </div>
-=======
             <p style={styles.details}>{product.shipping}</p>
->>>>>>> master
           </div>
         </div>
       </Fragment>
